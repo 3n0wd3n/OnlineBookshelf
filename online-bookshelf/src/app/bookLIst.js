baef38156a6d3354bfcdaf4d5119f8e1b2477354
Michael Hajný <CryptextCompany@gmail.com>
2025-02-03 21:26:00 +0100

    Update bookLIst.js

--- online-bookshelf/src/app/bookLIst.js
+++ online-bookshelf/src/app/bookLIst.js
@@ -3,6 +3,7 @@ import { FaExternalLinkAlt } from "react-icons/fa";
 
 const BookList = () => {
   return (
+    <ol className='list-inside list-decimal text-sm sm:text-left sm:pb-8 font-[family-name:var(--font-geist-mono)]'>
       {booksData.books.map((book, index) => (
         <li key={index} className='mb-2 flex items-center'>
           <a
