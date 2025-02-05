09a2d4ef88c727dae13c7c20c7fbc42a770c2df5
Michael Hajný <CryptextCompany@gmail.com>
2025-02-05 20:34:03 +0100

    update on json

--- online-bookshelf/src/app/bookLIst.js
+++ online-bookshelf/src/app/bookLIst.js
@@ -4,36 +4,48 @@ import { FaExternalLinkAlt } from "react-icons/fa";
 const BookList = ({ filter }) => {
   // Filter books based on the selected filter
   const filteredBooks = booksData.books.filter((book) => {
-    if (filter === "recommended") {
+    if (filter === "all") {
+      return (book.yetToBeRead === false) & (book.currRead === false);
+    } else if (filter === "recommended") {
       return book.recommended === true;
     } else if (filter === "iterative") {
       return book.iterative === true;
+    } else if (filter === "currRead") {
+      return book.currRead === true;
+    } else if (filter === "ytbr") {
+      return book.yetToBeRead === true;
+    } else {
+      return true;
     }
-    return true;
   });
+  console.log(filteredBooks.length);
 
   return (
     <ol className='list-inside list-decimal text-sm sm:text-left sm:pb-8 font-[family-name:var(--font-geist-mono)]'>
-      {filteredBooks.map((book, index) => (
-        <li key={index} className='mb-2 flex items-center'>
-          <a
-            href={book.link || undefined}
-            target='_blank'
-            rel='noopener noreferrer'
-            className={`inline-flex flex-col items-start md:flex-row gap-2 md:items-center ${
-              book.link ? "hover:underline cursor-pointer" : ""
-            }`}
-          >
-            <span className='font-bold'>
-              {index + 1}. {book.author}:
-            </span>{" "}
-            <span className='flex flex-row gap-2'>
-              {book.name}
-              {book.link && <FaExternalLinkAlt />}
-            </span>
-          </a>
-        </li>
-      ))}
+      {filteredBooks.length === 0
+        ? "Aktuálně nic nečtu"
+        : filteredBooks
+            .filter((book) => (filter === "currRead" ? book.currRead === true : book.currRead == false))
+            .map((book, index) => (
+              <li key={index} className='mb-2 flex items-center'>
+                <a
+                  href={book.link || undefined}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  className={`inline-flex flex-col items-start md:flex-row gap-2 md:items-center ${
+                    book.link ? "hover:underline cursor-pointer" : ""
+                  }`}
+                >
+                  <span className='font-bold'>
+                    {index + 1}. {book.author}:
+                  </span>{" "}
+                  <span className='flex flex-row gap-2'>
+                    {book.name}
+                    {book.link && <FaExternalLinkAlt />}
+                  </span>
+                </a>
+              </li>
+            ))}
     </ol>
   );
 };
