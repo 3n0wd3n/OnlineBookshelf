a5628f4f669bc2b39ea5c1adda0e3311d47f52ee
Michael Hajný <47132583+3n0wd3n@users.noreply.github.com>
2025-09-04 13:49:22 +0200

    Update page.js

--- online-bookshelf/src/app/page.js
+++ online-bookshelf/src/app/page.js
@@ -11,20 +11,6 @@ export default function Home() {
   const handleFilterChange = (filter) => {
     setFilter(filter);
   };
-
-  // Funkce pro zjištění stylu podle aktivního filtru
-  const getButtonClass = (btnFilter) => {
-    return `
-      cursor-pointer rounded-full border border-solid transition-colors flex items-center justify-center
-      text-sm sm:text-base h-10 sm:h-8 px-4 sm:px-4 w-full sm:w-auto
-      ${
-        filter === btnFilter
-          ? "bg-white text-black border-gray-500" // aktivní styl - bílý background, černý text a viditelný border
-          : "bg-foreground text-background border-transparent hover:bg-[#383838] dark:hover:bg-[#ccc]"
-      }
-    `;
-  };
-
   return (
     <div className='flex flex-col items-center justify-between w-full min-h-screen xxs:pt-5 xxs:pb-5 xxs:pl-5 xxs:pr-5 xs:pt-10 xs:pb-10 xs:pl-10 xs:pr-10 lg:pt-40 lg:pb-pb-10 lg:pl-60 lg:pr-60 md:pt-40 md:pb-pb-10 md:pl-40 md:pr-40 sm:pt-20 sm:pb-pb-10 sm:pl-20 sm:pr-20 font-[family-name:var(--font-geist-sans)]'>
       <main className='flex flex-col gap-8 items-start w-full'>
@@ -32,31 +18,31 @@ export default function Home() {
         <div className='flex gap-4 items-center flex-wrap sm:flex-row xxs:flex-col xxs:w-full'>
           <span
             onClick={() => handleFilterChange("all")}
-            className={getButtonClass("all")}
+            className='active:border-gray-500 cursor-pointer rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-8 px-4 sm:px-4 w-full sm:w-auto'
           >
             Přečtené
           </span>
           <span
             onClick={() => handleFilterChange("recommended")}
-            className={getButtonClass("recommended")}
+            className='active:border-gray-500 cursor-pointer rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-8 px-4 sm:px-4 w-full sm:w-auto'
           >
             Doporučuju
           </span>
           <span
             onClick={() => handleFilterChange("iterative")}
-            className={getButtonClass("iterative")}
+            className='active:border-gray-500 cursor-pointer rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-8 px-4 sm:px-4 w-full sm:w-auto'
           >
             Iterační
           </span>
           <span
             onClick={() => handleFilterChange("currRead")}
-            className={getButtonClass("currRead")}
+            className='active:border-gray-500 cursor-pointer rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-8 px-4 sm:px-4 w-full sm:w-auto'
           >
             Aktuálně čtu
           </span>
           <span
             onClick={() => handleFilterChange("ytbr")}
-            className={getButtonClass("ytbr")}
+            className='active:border-gray-500 cursor-pointer rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-8 px-4 sm:px-4 w-full sm:w-auto'
           >
             Na seznamu
           </span>
